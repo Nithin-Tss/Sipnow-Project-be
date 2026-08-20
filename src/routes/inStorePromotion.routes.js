10cfe696d0764be8ed30a07784d3fc24d4418fde
Nithin-Tss <Nithin.B@techstalwartsolution.com>
2026-08-20 17:02:41 +0530

    fix: replace optionalAuth with requireAuth in inStorePromotion routes

--- src/routes/inStorePromotion.routes.js
+++ src/routes/inStorePromotion.routes.js
@@ -7,7 +7,7 @@ const {
   update,
   remove,
 } = require("../controllers/inStorePromotion.controller");
-const { optionalAuth, requireAdmin } = require("../middleware/auth");
+const { requireAuth, requireAdmin } = require("../middleware/auth");
 
 const router = Router();
 
@@ -15,8 +15,8 @@ router.get("/", list);
 router.get("/active", getActivePromotions);
 router.get("/:id", getOne);
 
-router.post("/", optionalAuth, requireAdmin, create);
-router.put("/:id", optionalAuth, requireAdmin, update);
-router.delete("/:id", optionalAuth, requireAdmin, remove);
+router.post("/", requireAuth, requireAdmin, create);
+router.put("/:id", requireAuth, requireAdmin, update);
+router.delete("/:id", requireAuth, requireAdmin, remove);
 
 module.exports = router;
